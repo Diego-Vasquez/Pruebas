aa9a753a68c49af7f0cd2bffc82a4dd4472e3024
Diego-Vasquez <diego.vasquez.l@uni.pe>
2022-02-21 11:34:51 -0500

    Nuevos cambios

--- directorio_prueba/my_library/__manifest__.py
+++ directorio_prueba/my_library/__manifest__.py
@@ -5,7 +5,7 @@
     Libreria de prueba
     """,
     'author': "DiegoV",
-    'depends': ['base'],
+    'depends': ['base', 'contacts'],
     'data': ['security/groups.xml',
             'security/ir.model.access.csv',
             'security/security_rules.xml',
@@ -15,6 +15,8 @@
             'views/library_book_return_wizard.xml',
             'data/data.xml',
             'data/demo.xml',
+            'reports/book_rent_report.xml',
+            'reports/book_rent_templates.xml',
              ],
     'post_init_hook': 'add_book_hook',
     'demo': [
